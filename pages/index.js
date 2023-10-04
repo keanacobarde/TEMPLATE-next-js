882ef89ec08096d6462f02740f09df1788ca0e28
Keana Cobarde <keanacobarde@gmail.com>
2023-10-03 23:05:24 -0500

    getting counter to work using cbs within onClick event handler. received same error when attempting to generalize within handleClick function

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { useState } from 'react';
 
 function Home() {
   // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  const user = { displayName: 'Kiki' }; // TODO: COMMENT OUT FOR AUTH
+  const [counterValue, counter] = useState(0);
+
+  const handleClick = () => {
+    counter(counterValue + 1);
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -17,6 +24,10 @@ function Home() {
       }}
     >
       <h1>Hello {user.displayName}! </h1>
+      <h1> {counterValue} </h1>
+      <button type="button" onClick={handleClick}> Increment </button>
+      <button type="button" onClick={() => counter(counterValue - 1)}> Decrement </button>
+      <button type="button" onClick={() => counter(counterValue - counterValue)}> Reset </button>
     </div>
   );
 }
