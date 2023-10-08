cf8bcc76e776f2262e1440fe855d5ab18a8b0ebb
Keana Cobarde <keanacobarde@gmail.com>
2023-10-08 11:57:32 -0500

    modifying handleClick event to utilize switch cases and modified onClick to utilize anon func

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
-import { useState } from 'react';
+import Counter from '../components/Counter';
 
 function Home() {
   // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
-
   const user = { displayName: 'Kiki' }; // TODO: COMMENT OUT FOR AUTH
-  const [counterValue, counter] = useState(0);
-
-  const handleClick = () => {
-    counter(counterValue + 1);
-  };
-
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -24,10 +17,7 @@ function Home() {
       }}
     >
       <h1>Hello {user.displayName}! </h1>
-      <h1> {counterValue} </h1>
-      <button type="button" onClick={handleClick}> Increment </button>
-      <button type="button" onClick={() => counter(counterValue - 1)}> Decrement </button>
-      <button type="button" onClick={() => counter(counterValue - counterValue)}> Reset </button>
+      <Counter title={"Keana's Counter"} />
     </div>
   );
 }
